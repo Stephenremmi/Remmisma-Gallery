b8f6260ea55fe7f61f5e44f0e9a9148cde28298d
stephen remmi <stephenremmi@gmail.com>
2020-06-29 02:21:53 +0300

    Tweak code in settings.py

--- gallery_project/settings.py
+++ gallery_project/settings.py
@@ -127,5 +127,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
